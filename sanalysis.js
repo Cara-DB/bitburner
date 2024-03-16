b400e6ca850e2eca80f04df9d296951809c82b82
Cara-DB <73352268+Cara-DB@users.noreply.github.com>
2024-03-16 16:20:12 +0000

    Add files via upload

--- sanalysis.js
+++ sanalysis.js
@@ -3,6 +3,7 @@ function Server(name, level, money, growth) {
   this.hackLevel = level;
   this.maxMoney = money;
   this.growth = growth;
+  this.levelNeeded = level * 2;
   this.money = function () {
     return this.maxMoney;
   };
@@ -31,20 +32,26 @@ export async function main(ns) {
   let formatted = [];
   let hostnames = ['home'];
   let targets = [];
+  let servert = [];
   for (let e = 0; e < hostnames.length; e++) {
     hostnames.push(...ns.scan(hostnames[e]).filter(hostname => !hostnames.includes(hostname)))
   }
   for (let i in hostnames) {
-    if (ns.getServerRequiredHackingLevel(hostnames[i]) < (ns.getHackingLevel() /2) && ns.getServerMaxMoney(hostnames[i]) > 10000000) {
+    if (ns.getServerRequiredHackingLevel(hostnames[i]) < (ns.getHackingLevel() / 2) && ns.getServerMaxMoney(hostnames[i]) > 10000000) {
+      servert.push(new Server(hostnames[i], ns.getServerRequiredHackingLevel(hostnames[i]), ns.getServerMaxMoney(hostnames[i]), ns.getServerGrowth(hostnames[i])));
+    }
+    if (ns.getServerRequiredHackingLevel(hostnames[i]) < (ns.getHackingLevel()) && ns.getServerMaxMoney(hostnames[i]) > 10000000) {
       servers.push(new Server(hostnames[i], ns.getServerRequiredHackingLevel(hostnames[i]), ns.getServerMaxMoney(hostnames[i]), ns.getServerGrowth(hostnames[i])));
     }
   }
-  const output = await ns.prompt("Please select sorting method:", {
-    type: "select",
-    choices: ["serverName", "hackLevel", "maxMoney", "growth", "growth"]
-  });
-  serverinfo = sortServers(servers, output);
-  const servert = sortServers(servers, "maxMoney");
+  if (ns.args.length != 0) {
+    const output = await ns.prompt("Please select sorting method:", {
+      type: "select",
+      choices: ["serverName", "hackLevel", "maxMoney", "growth", "levelNeeded"]
+    });
+    serverinfo = sortServers(servers, output);
+  }
+  servert = sortServers(servert, "maxMoney");
   for (let i in serverinfo) {
     let change = serverinfo[i].money();
     serverinfo[i].changeMoney(ns.formatNumber(change));
