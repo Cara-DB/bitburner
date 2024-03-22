75adb656bff7033e9258d8533a6b0527adb56088
Cara DB <cara.db@outlook.com>
2024-03-22 13:01:56 +0000

    split targets evenly

--- batch/binital.js
+++ batch/binital.js
@@ -3,9 +3,11 @@ export async function main(ns) {
   let targets = ns.read("targets.txt");
   const servers = ns.getPurchasedServers();
   targets = targets.split("\r\n");
+  let secondTarget;
   if (targets.length > 25) {
-    const secondTarget = targets.slice(25);
-    targets = targets.slice(0, 25);
+    const split = Math.floor(targets.length / 2);
+    secondTarget = targets.slice(split);
+    targets = targets.slice(0, split);
   }
   let t = 0
   for (let i in servers) {
@@ -20,15 +22,14 @@ export async function main(ns) {
     ns.scp("jhack.js", servers[i]);
     ns.scp("jweaken.js", servers[i]);
     let count = Math.floor(ns.getServerMaxRam(servers[i]) / 200000);
-    if (count > 1) {
-      for (let e = 0; e < (count / 2); e++) {
-        if (t > targets.length - 1) {
-          t = 0;
-        }
-        ns.exec("batch/bserver.js", servers[i], 1, targets[t], 200000);
-        await ns.sleep(20);
-        ns.exec("batch/bserver.js", servers[i], 1, secondTarget[i], 200000);
+    if (count > 2) {
+      let ram = Math.floor(ns.getServerMaxRam(servers[i]) / 2);
+      if (t > targets.length - 1) {
+        t = 0;
       }
+      ns.exec("batch/bserver.js", servers[i], 1, targets[t], ram);
+      await ns.sleep(20);
+      ns.exec("batch/bserver.js", servers[i], 1, secondTarget[t], ram);
     }
     else {
       ns.exec("batch/bserver.js", servers[i], 1, targets[t], ns.getServerMaxRam(servers[i]), 1);
