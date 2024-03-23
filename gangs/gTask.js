c1c9c0bd7e95e7b6936b99c2ea1bdcae0738cfa9
Cara DB <cara.db@outlook.com>
2024-03-23 15:36:39 +0000

    split gTrain into its own thing

--- gangs/gTask.js
+++ gangs/gTask.js
@@ -5,12 +5,7 @@
  * - Group members by task
  * - Task Assignment:
  *  - Move members from training when they're ready
- *  - At least [variable] members in each group:
- *    - Crime
- *    - Vigilante Justice
- *    - Extra training (for ascention)
- *    - Terrorism
- *    - RATIO: 3/2/1/1
+ *  - Mode based on arg?
  */
 /** @param {NS} ns */
 function Stats(member) { // How much exp a member should have
@@ -31,43 +26,45 @@ function Stats(member) { // How much exp a member should have
 /** @param {NS} ns */
 export async function main(ns) {
   const host = ns.getHostname();
-  while (true) {
-    let members = ns.gang.getMemberNames();
-    // Groups vvv
-    let training = [];
-    let extraTraining = [];
-    let commitingCrime = [];
-    let commitingJustice = [];
-    let commitingTerror = [];
-    let uncommited = [];
-    for (let i in members) { // Group members
-      let memInfo = ns.gang.getMemberInformation(members[i]);
-      if (memInfo.task === "Train Combat" || memInfo.task === "Train Hacking" || memInfo.task === "Train Charisma") {
-        let check = new Stats(memInfo);
-        if (check.enoughCharisma() && check.enoughCombat() && check.enoughHacking()) {
-          extraTraining.push(members[i]);
-        }
-        else {
-          training.push(members[i]);
-        }
-      }
-      else if (memInfo.task === "Human Trafficking") {
-        commitingCrime.push(members[i]);
-      }
-      else if (memInfo.task === "Terrorism") {
-      commitingTerror.push(members[i]);
-      }
-      else if (memInfo.task === "Vigilante Justice") {
-      commitingJustice.push(members[i]);
+  let members = ns.gang.getMemberNames();
+  // Groups vvv
+  let training = [];
+  let extraTraining = [];
+  let commitingCrime = [];
+  let commitingJustice = [];
+  let commitingTerror = [];
+  let territory = [];
+  let uncommited = [];
+  for (let i in members) { // Group members
+    let memInfo = ns.gang.getMemberInformation(members[i]);
+    if (memInfo.task === "Train Combat" || memInfo.task === "Train Hacking" || memInfo.task === "Train Charisma") {
+      let check = new Stats(memInfo);
+      if (check.enoughCharisma() && check.enoughCombat() && check.enoughHacking()) {
+        extraTraining.push(members[i]);
       }
       else {
-      uncommited.push(members[i]);
+        training.push(members[i]);
       }
+    }
+    else if (memInfo.task === "Human Trafficking") {
+      commitingCrime.push(members[i]);
+    }
+    else if (memInfo.task === "Terrorism") {
+      commitingTerror.push(members[i]);
+    }
+    else if (memInfo.task === "Vigilante Justice") {
+      commitingJustice.push(members[i]);
+    }
+    else if (memInfo.task === "Territory Warfare") {
+      territory.push(members[i]);
+    }
+    else {
+      uncommited.push(members[i]);
+    }
   }
   if (training.length > 0) { // Helps members train if needed
     ns.exec("gangs/gTrain.js", host, 1, ...training);
   }
   let ratio = Math.floor(members.length / 6);
   await ns.sleep(10000);
-}
 }
