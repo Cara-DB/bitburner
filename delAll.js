9f732d79109dfda5c8fdda6889c9513a6e9ac715
Cara DB <cara.db@outlook.com>
2024-03-23 16:40:39 +0000

    minor formatting

--- delAll.js
+++ delAll.js
@@ -3,6 +3,6 @@ export async function main(ns) {
   const servers = ns.getPurchasedServers();
   for (let i in servers) {
     ns.killall(servers[i]);
-    ns.deleteServer(servers[i])
+    ns.deleteServer(servers[i]);
   }
 }
