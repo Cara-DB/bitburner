00dbf52e79da0275492c5b183f4117cea645d3a0
Cara DB <cara.db@outlook.com>
2024-03-24 14:57:15 +0000

    now runs simplehack.js on purchased servers

--- purchaseServer.js
+++ purchaseServer.js
@@ -1,11 +1,13 @@
 /** @param {NS} ns */
 export async function main(ns) {
-  const ram = 4;
+  const ram = 8;
   let i = 0;
   while (i < ns.getPurchasedServerLimit()) {
     if (ns.getServerMoneyAvailable("home") > ns.getPurchasedServerCost(ram)) {
       ns.purchaseServer("pserv-" + i, ram);
       ++i;
+      ns.scp("simpleHack.js", "pserv-" + i);
+      ns.exec("simpleHack.js", "pserv-" + i, 3, "n00dles");
     }
     await ns.sleep(50);
   }
