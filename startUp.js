e349b7d760240e9010b0e6629cc2574ee28f306b
Cara DB <cara.db@outlook.com>
2024-03-24 14:57:41 +0000

    Quick start post-reload

---
+++ startUp.js
@@ -0,0 +1,9 @@
+/**
+ * startUp.js - Quickstart
+ */
+/** @param {NS} ns */
+export async function main(ns) {
+    ns.nuke("n00dles");
+    ns.exec("hhack.js", "home", 1, "n00dles");
+    ns.exec("purchaseServer.js", "home");
+}
